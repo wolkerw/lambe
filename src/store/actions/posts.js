fcdcf9bff70ff3d1453f47cdd7e499e6fc848d3c
Wolker <wolker@localdrive.com.br>
2019-06-20 17:14:51 -0300

    Posts - saving to the Firebase

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -1,11 +1,18 @@
 import { ADD_POST, ADD_COMMENT } from './actionTypes'
+import axios from 'axios'
 
 // passa post por parâmetro e retorna post pelo payload
 export const addPost = post => {
-  return {
-    type: ADD_POST,
-    payload: post
+  return dispatch => {
+    // armazena em um arquivo posts.json
+    axios.post('/posts.json', { ...post })
+      .catch(err => console.log(err))
+      .then(res => console.log(res.data))
   }
+  // return {
+  //   type: ADD_POST,
+  //   payload: post
+  // }
 }
 
 export const addComment = payload => {
